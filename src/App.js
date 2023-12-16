baa7fb99225400d7ad98121f3a4b528a0082ab90
Nirzon Karmakar <nirzon47@gmail.com>
2023-12-16 19:59:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header/Header'
+
+import Footer from './Footer/Footer'
+
+const App = () => {
+	return (
+		<div className='relative'>
+			<Header />
+
+			<Footer />
+		</div>
+	)
+}
+
+export default App
