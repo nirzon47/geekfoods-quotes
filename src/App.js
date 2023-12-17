1c26707a43cad97c1d7441c614015dcc8f05f566
Nirzon Karmakar <nirzon47@gmail.com>
2023-12-17 17:30:27 +0530

    added quote component with quoteItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from './Header/Header'
-
 import Footer from './Footer/Footer'
+import Quotes from './Quotes/Quotes'
 
 const App = () => {
 	return (
 		<div className='relative'>
 			<Header />
-
+			<Quotes />
 			<Footer />
 		</div>
 	)
